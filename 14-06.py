458d3ad3117feb468db055e06bfc7212d0d9fe9a
JSB-007 <125993110+JSB-007@users.noreply.github.com>
2023-06-16 11:34:24 +0530

    Update 14-06.py

--- 14-06.py
+++ 14-06.py
@@ -1,28 +1,44 @@
-import boto3
-import pandas as pd
-from smart_open import open
+12: code walkthrough
+13: ddl_compare
+14: code for conversion of file from Linux server(paramiko) and S3 bucket(boto3) into dataframe 
+15: code for connector for Linux server and S3 bucket to python
+16: raised access request for AWS credentials and Linux server
 
-# Set up AWS credentials
-access_key = 'YOUR_ACCESS_KEY'
-secret_key = 'YOUR_SECRET_KEY'
-bucket_name = 'YOUR_BUCKET_NAME'
-file_key = 'YOUR_FILE_KEY'  # The key or path of the file within the bucket
+Next week: start testing of the code functionality as soon as we receive db access
 
-# Create a session using your AWS credentials
-session = boto3.Session(
-    aws_access_key_id=access_key,
-    aws_secret_access_key=secret_key
-)
+First and foremost, I am happy to report that our project is progressing well and remains on track to meet its objectives and deadlines. We have achieved several important milestones and have overcome various challenges along the way. 
 
-# Create an S3 client
-s3_client = session.client('s3')
 
-# Read the file from S3 into a DataFrame
-s3_uri = f's3://{bucket_name}/{file_key}'
-with open(s3_uri, 'rb') as file:
-    df = pd.read_csv(file)
+Progress and Support at TIAA
 
-# Now you can work with the DataFrame
-print(df.head())
 
-#
+User
+Write a answer to give to director about how we are progressing in our project, how this project is increasing our knowledge, and how the mentor and the complete team of TIAA is helping us with the project and making us comfortable in the new environment 
+
+ChatGPT
+Dear Director,
+
+I am pleased to provide you with an update on the progress of our project and highlight the valuable insights and knowledge we have gained through its execution. Moreover, I would like to emphasize the significant support we have received from our mentor and the entire team at TIAA, which has contributed to our success and made us feel comfortable in the new environment.
+
+First and foremost, I am happy to report that our project is progressing well and remains on track to meet its objectives and deadlines. We have achieved several important milestones and have overcome various challenges along the way. The dedication and hard work of our team members have been instrumental in ensuring smooth progress and maintaining project momentum.
+
+In terms of knowledge gained, this project has been an incredible learning opportunity for all of us. As we delve deeper into its intricacies, we are continuously expanding our understanding of the subject matter and developing new skills. The project has exposed us to cutting-edge technologies, innovative methodologies, and best practices in the industry. This experience has not only enhanced our technical expertise but also broadened our perspectives, enabling us to think critically and creatively in solving complex problems.
+
+Progress and Support at TIAA
+
+
+User
+Write a answer to give to director about how we are progressing in our project, how this project is increasing our knowledge, and how the mentor and the complete team of TIAA is helping us with the project and making us comfortable in the new environment 
+
+ChatGPT
+Dear Director,
+
+I am pleased to provide you with an update on the progress of our project and highlight the valuable insights and knowledge we have gained through its execution. Moreover, I would like to emphasize the significant support we have received from our mentor and the entire team at TIAA, which has contributed to our success and made us feel comfortable in the new environment.
+
+First and foremost, I am happy to report that our project is progressing well and remains on track to meet its objectives and deadlines. We have achieved several important milestones and have overcome various challenges along the way. The dedication and hard work of our team members have been instrumental in ensuring smooth progress and maintaining project momentum.
+
+In terms of knowledge gained, this project has been an incredible learning opportunity for all of us. As we delve deeper into its intricacies, we are continuously expanding our understanding of the subject matter and developing new skills. The project has exposed us to cutting-edge technologies, innovative methodologies, and best practices in the industry. This experience has not only enhanced our technical expertise but also broadened our perspectives, enabling us to think critically and creatively in solving complex problems.
+
+The mentor assigned to our team has been invaluable in guiding us throughout the project. Their vast experience and deep domain knowledge have been instrumental in providing us with direction and insights, helping us navigate through uncertainties and make informed decisions. They have been readily available to address our queries, provide constructive feedback, and offer guidance whenever needed. 
+
+Furthermore, the support we have received from the entire team at TIAA has been exceptional. Our colleagues have been incredibly collaborative and always willing to lend a helping hand. This collective effort has created a positive and inclusive work environment, making us feel comfortable and supported in our new surroundings.
