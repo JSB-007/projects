637079d2b9990891294e2f20b0802f78f11d94fa
JSB-007 <125993110+JSB-007@users.noreply.github.com>
2023-07-20 15:53:13 +0530

    Update 14-06.py

--- 14-06.py
+++ 14-06.py
@@ -1,1 +1,11 @@
+Hi Dhanshree,
 I wanted to inform you that I haven't received any communication regarding the scheduling of the interview or any updates on the PPO selection process. I understand that the HR team might be busy with numerous applications and other responsibilities, but I wanted to ensure that I haven't missed any important notifications.
+
+I remain enthusiastic about the prospect of joining TIAA, and I am genuinely interested in contributing my skills and experiences to the team.
+
+Thank you for your time and attention to this matter. I look forward to hearing from you soon.
+
+Thanks and regards,
+
+Jasmeet Bhatia
+Jasmeet.Bhatia@in.tiaa.org
