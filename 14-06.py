ff5c5cb4e5bd7b66831b05741566301eef76dfdb
JSB-007 <125993110+JSB-007@users.noreply.github.com>
2024-02-03 12:56:35 +0530

    Update 14-06.py

--- 14-06.py
+++ 14-06.py
@@ -1,11 +1,39 @@
-Hi Dhanshree,
-I wanted to inform you that I haven't received any communication regarding the scheduling of the interview or any updates on the PPO selection process. I understand that the HR team might be busy with numerous applications and other responsibilities, but I wanted to ensure that I haven't missed any important notifications.
+Week 1:
+•	08-Jan: Initiated the onboarding process, acquainting with organizational culture and protocols.
+•	09-Jan: Engaged in comprehensive Workday training sessions to enhance operational efficiency.
+•	10-Jan: Participated in first team meeting, fostering collaboration and communication, and developed functionality of the team in the organization
+•	11-Jan: Attended a project overview session with Rajiv, gaining insights into the project’s significance for both the company and team.
+•	12-Jan: Delved into the project details through PPTs and confluence pages, enhancing understanding.
+Week 1 Learnings:
+-	Acquired foundational knowledge of organizational operations and team dynamics.
+-	Gained insights into the pivotal role of our upcoming project in driving organizational success.
 
-I remain enthusiastic about the prospect of joining TIAA, and I am genuinely interested in contributing my skills and experiences to the team.
+Week 2:
+•	15-Jan: Initiated necessary requests for project resources including Gitlab, ALM etc., ensuring readiness for execution.
+•	16-Jan: Explored UI and API testing intricacies with Ayush; learned Jira, ALM operations enhancing technical proficiency.
+•	17-Jan: Utilized confluence articles for PDM - Project setup, ensuring alignment with operational standards.
+•	18-Jan: Deepened understanding of PDM modules to enhance execution efficiency.
+•	19-Jan: Explored CodeceptJS through articles & YouTube videos; enhanced scripting knowledge.
+Week 2 Learnings:
+-	Developed an understanding of various tools & platforms integral to project execution.
+-	Enhanced technical acumen through interactive learning sessions.
 
-Thank you for your time and attention to this matter. I look forward to hearing from you soon.
+Week 3:
+•	22-Jan: Mastered JIRA, ALM operations via instructional YouTube videos; optimized task management skills.
+•	23-Jan: Acquired proficiency in Postman, Insomnia & API testing environments (ST2 & ST4) via YouTube tutorials.
+•	24-Jan: Grasped key testing terminologies like regression & Blackbox testing; understood scrum methodologies enhancing agile capabilities.
+•	25-Feb: Collaborated with Shravani to understand Data runner enhancements requirements.
+Week 3 Learnings: 
+- Mastered various software tools enhancing operational efficiency. 
+- Fostered collaborative skills while working on Data runner enhancements.
 
-Thanks and regards,
-
-Jasmeet Bhatia
-Jasmeet.Bhatia@in.tiaa.org
+Week 4:
+•	29-Jan: Set up Python and the project for Data runner, laying the groundwork for development.
+•	30-Jan: Familiarized with the Data runner codebase, enhancing understanding of the project’s technical aspects.
+•	31-Jan: Received hands-on training from Ayush, furthering practical knowledge of the project.
+•	01-Feb: Collaborated with Udit to understand the details of the BRS project, specifically PDF comparison.
+•	02-Feb: Raised a request for access of the PDM UI and conducted research on the problem statement for the BRS project.
+Week 4 Learnings: 
+- Gained hands-on experience with the project, enhancing practical skills. 
+- Fostered collaborative skills while working on the BRS project. 
+- Developed a deeper understanding of the project’s technical and operational aspects
